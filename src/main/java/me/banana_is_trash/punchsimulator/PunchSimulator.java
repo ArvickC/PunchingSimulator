88cd7c24303d7cfc94f2496de1a5d24032658b62
ArvickC <arvick@outlook.com>
2020-11-30 11:08:59 -0800

    First Commit

---
+++ src/main/java/me/banana_is_trash/punchsimulator/PunchSimulator.java
@@ -0,0 +1,17 @@
+package me.banana_is_trash.punchsimulator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PunchSimulator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
